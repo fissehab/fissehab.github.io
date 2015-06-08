5e60c948b91b3e2f13c5cd29811755efdbe5c5f4
fissehab <fishjhu@gmail.com>
2015-06-07 22:52:06 -0400

    style changed

---
+++ R/style.js
@@ -0,0 +1,23 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href="index.html">Home</a></li>"+
+"<li><a href="education.html">Education</a></li>"+
+  "<li><a href="interests.html">interests</a></li>"+
+" <li><a href="Experience.html">Experience</a></li>"+
+
+ "<li><a href="skills.html">Skills</a></li>"+
+
+  "<li><a href="publications.html">Publications</a></li>"+
+"<li><a href="presentations.html">Presentations</a></li>"
+  "<li><a href="awards.html">Awards</a></li>"+
+   "<li><a href="python.html">Python</a></li>"+
+  "<li><a href="R.html">R</a></li>"+
+  "<li><a href="Matlab.html">Matlab</a></li>"+
+  "<li><a href="scomputing.html">Statistical Computing</a></li>"+
+  "<li><a target="_blank" href="certificates.html">Certificates</a></li>"+
+  "<li><a target="_blank" href="mlresources.html">ML Resources</a></li>"+
+  "</ul>"; 
+  
+  document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Fisseha Berhane <fisseha@jhu.edu. All rights reserved.</p>";
+
