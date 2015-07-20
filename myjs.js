389ac3afa68e08b03001c5a2f6f79a2db218c3e6
fissehab <fishjhu@gmail.com>
2015-07-20 12:57:59 -0400

    js for icons added

---
+++ myjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+						   
+  $(".icon").mouseenter(function() {
+    $(this).css("background-color","#FF99C2");
+  });
+  $(".icon").mouseleave(function() {
+    $(this).css("background-color","#CCE680");
+  });
+  
+});
